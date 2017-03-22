41139dca1fdb9fbc352f5e72e96ef7af19aaa82e
liberations <wenxianglimail@126.com>
2017-03-22 09:11:46 +0800

    sensor && meida

--- device.mk
+++ device.mk
@@ -189,6 +189,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media_codecs_qiku.xml:system/etc/media_codecs_qiku.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:system/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
 
